8f8cc35144e793f07bf8a41563add7d9b348c12c
thanhnh-infinity <thanhnh2911@gmail.com>
2018-05-19 00:19:08 -0600

    Add XHAIL

---
+++ XHAIL/src/main/java/xhail/core/terms/Term.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package xhail.core.terms;
+
+
+/**
+ * @author stefano
+ *
+ */
+public interface Term {
+
+	// public Collection<Term> matches(SchemeTerm term, Collection<Term> usables, Set<Atom> facts);
+
+}
